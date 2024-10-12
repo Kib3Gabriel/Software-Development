829d6c2812be2f5489829e4d4a8bb183e062f18b
gabriel <gkibe12@gmail.com>
2024-10-12 13:08:36 +0300

    xata

--- 3.Node/Assignments/ZXata/src/server.ts
+++ 3.Node/Assignments/ZXata/src/server.ts
@@ -101,40 +101,6 @@ app.delete('/user/:email', async (req: Request, res: Response) => {
 
 
 
-// app.put('/user/update/:name', async (req: Request, res: Response) => {
-//     const personName: string = req.params.name.toLowerCase();
-//     const { email, password, name } = req.body;
-
-//     if (!email && !password && !name) {
-//         return res.status(400).json({ message: 'At least one field (name, email, or password) is required for an update.' });
-//     }
-
-//     try {
-//         const personData = await client.db.personInfo.getAll();
-//         const foundPerson = personData.find(person => person.name?.toLowerCase() === personName);
-
-//         if (!foundPerson) {
-//             return res.status(404).json({ message: 'Person not found' });
-//         }
-
-//         // Update fields only if they are provided in the request body
-//         const updatedPersonData = {
-//             email: email ?? foundPerson.email,
-//             password: password ?? foundPerson.password, // Consider hashing the password
-//             name: name ?? foundPerson.name
-//         };
-
-//         // Update the person’s information in the Xata database
-//         await client.db.personInfo.update(foundPerson.id, updatedPersonData);
-//         res.status(200).json({ message: 'User updated successfully', updatedPersonData });
-        
-//     } catch (error) {
-//         console.error(`Error updating data for person ${personName}: ${error}`);
-//         res.status(500).json({ message: 'Server error while updating user' });
-//     }
-// });
-
-
 
 app.listen(PORT, async() =>{
     console.log(`Server is running at http://localhost:${PORT}`);
